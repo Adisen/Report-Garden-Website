385887f7c2493cc9ed56465e943cdece7726b814
Israel Adetunji <adetunjiisraeleniola@yahoo.com>
2018-04-04 16:04:20 +0100

    Added some styles to the desktop view

---
+++ main.js
@@ -0,0 +1,10 @@
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+
+// console.log (menu);
+// console.log(nav);
+
+menu.addEventListener('onclick', function(){
+    nav.style.display = 'block';
+    console.log('112');
+});
